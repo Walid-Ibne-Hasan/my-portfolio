9d79cad0ec6f6f2fd9822edef31dd9280060ebae
WALID IBNE HASAN <20301103@bracu.ac.bd>
2023-01-29 13:58:22 +0600

    olala

---
+++ assignment1.js
@@ -0,0 +1,4 @@
+document.querySelector(".submit-btn").addEventListener("click",function(){
+    document.querySelector("input-txt-thought").textContent="Thanks for the response"
+})
+console.log("olala");
